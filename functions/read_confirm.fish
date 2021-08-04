64fff60dacbfead1d1871abf4e0bcce0359c6535
Francis Chua <francis@opennetworking.org>
2021-08-04 09:48:16 -0700

    formatting

---
+++ functions/read_confirm.fish
@@ -0,0 +1,12 @@
+function read_confirm -d "A helper function to do a boolean confirm"
+  while true
+    read -l -P 'Do you want to continue? [y/N] ' confirm
+
+    switch $confirm
+      case Y y
+        return 0
+      case '' N n
+        return 1
+    end
+  end
+end
